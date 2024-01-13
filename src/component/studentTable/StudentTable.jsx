2ad8f48aac08032639dd4e86af39e56f117bb034
kumarcmcc <63100682+kumarcmcc@users.noreply.github.com>
2024-01-14 02:04:44 +0900

    Update StudentTable.jsx css Class 7line

--- src/component/studentTable/StudentTable.jsx
+++ src/component/studentTable/StudentTable.jsx
@@ -4,7 +4,7 @@ import Table from './Table'
 export default function StudentTable() {
   return (
     <section className="py-24 lg:pt-[120px] lg:pb-28">
-            <div className="container">
+            <div className="container m-auto">
                 <div className="mb-16 flex flex-col items-center">
                     <h2 className="text-3xl lg:text-[40px] mb-9 font-bold">
                         <span className="text-[#00CC8C]">Students</span> of the Year
